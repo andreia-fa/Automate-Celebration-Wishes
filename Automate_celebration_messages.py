2448610dc14f22ba70df270dd2067665045937e5
aalmeida <almeidandreia@gmail.com>
2024-09-08 19:50:24 +0200

    checking message sent and not to repeating it for the diferent events.

--- Automate_celebration_messages.py
+++ Automate_celebration_messages.py
@@ -11,104 +11,158 @@ class Automate_messages:
         self.session_file_name = session_file_name
         self.app_id = app_id
         self.app_hash = app_hash
-        self.sql_connection = sql_connection
+        self.sql_connection = sql_connection  
 
     def should_event_message_be_sent(self, mysql_cnx, contacts_table, messages_table):
-        contacts_with_events = self.sql_connection.get_contacts_with_event_types(mysql_cnx, contacts_table)
+        today_month_day = self.get_today_date_details()
+        
+        contacts_with_events = self.sql_connection.get_contacts_for_today_events(mysql_cnx, contacts_table, today_month_day)
         if not contacts_with_events:
-            logging.info("No contact information available.")
-            return "No contact information available."
-
-        today = datetime.now().strftime('%Y-%m-%d')
-        today_day = datetime.now().strftime('%d')  # Day for monthly events
-        logging.debug(f"Today's date: {today}")
-        logging.debug(f"Today's day: {today_day}")
-
-        events_today = []
-        for name, (event_date, message_rec, recurrence, event_type) in contacts_with_events.items():
-            # Check if event_date is a string and convert it to a datetime object
-            if isinstance(event_date, str):
-                try:
-                    event_date_obj = datetime.strptime(event_date, '%Y-%m-%d')
-                except ValueError as e:
-                    logging.error(f"Error parsing date for {name}: {e}")
-                    continue  # Skip this record if the date format is incorrect
-            else:
-                event_date_obj = event_date  # If already a datetime object
-
-            event_date_full = event_date_obj.strftime('%Y-%m-%d')
-            event_date_day = event_date_obj.strftime('%d')
-
-            logging.debug(f"Checking {name}: Event date: {event_date}, Recurrence: {recurrence}, Event type: {event_type}")
-
-            if recurrence == 'Monthly' and today_day == event_date_day:
-                events_today.append((name, message_rec, event_type))
-                logging.info(f"Monthly event today for {name}: {event_date}")
-            elif recurrence == 'Annual' and today == event_date_full:
-                events_today.append((name, message_rec, event_type))
-                logging.info(f"Annual event today for {name}: {event_date}")
-
-        if not events_today:
-            logging.info("No events today.")
-            return "No events today."
-
-        for name, message_rec, event_type in events_today:
-            if not self.has_message_been_sent(mysql_cnx, name, event_type):
-                celebration_message = self.sql_connection.get_event_message(mysql_cnx, messages_table, event_type)
-                if not celebration_message:
-                    logging.error(f"No message found for event type: {event_type}")
-                    continue
-
-                logging.info(f"Sending {event_type} message to {name}. Message: {celebration_message}")
-                send_status = self.send_msg(message_rec, celebration_message)
-                if send_status == "Success":
-                    self.log_message_sent(mysql_cnx, name, event_type, celebration_message)
-                    logging.info(f"Sent {event_type} message to {name}: {celebration_message}")
+            logging.info("No contacts with events today.")
+            return "No contacts with events today!!!"
+        
+        for name, (event_date, phone_number, recurrence, event_type) in contacts_with_events.items():
+            # Get the last sent message for the user
+            last_message_text = self.get_last_sent_message(mysql_cnx, name)
+            
+            # Fetch a message excluding the last sent one
+            message_id, celebration_message = self.sql_connection.get_event_message(mysql_cnx, messages_table, event_type, last_message_text)
+            
+            if celebration_message:
+                if not self.has_message_been_sent(mysql_cnx, name, message_id, event_type):
+                    logging.info(f"Sending {event_type} message to {name}. Message: {celebration_message}")
+                    send_status = self.send_msg(name, phone_number, celebration_message)
+                    if send_status == "Success":
+                        self.log_message_sent(mysql_cnx, name, celebration_message)
+                        logging.info(f"Sent {event_type} message to {name}: {celebration_message}")
+                    else:
+                        logging.error(f"Failed to send {event_type} message to {name}")
                 else:
-                    logging.error(f"Failed to send {event_type} message to {name}")
+                    logging.info(f"{event_type.capitalize()} message (ID: {message_id}) to {name} has already been sent.")
             else:
-                logging.info(f"{event_type.capitalize()} message to {name} has already been sent this year.")
+                logging.error(f"No message found for event type: {event_type}")
 
         return "Event messages processed."
 
 
-    def has_message_been_sent(self, cnx, person_name, event_type):
+    def get_today_date_details(self):
+        today = datetime.now()
+        #today_date_str = today.strftime('%Y-%m-%d')
+        #today_day = today.strftime('%d')
+        today_month_day = today.strftime('%m-%d')
+        return today_month_day
+
+    
+    def has_message_been_sent(self, cnx, person_name, message_id, event_type):
         cursor = cnx.cursor()
         current_year = datetime.now().year
-        query = """
-        SELECT COUNT(*)
-        FROM message_log
-        WHERE Username = %s AND YEAR(date_sent) = %s
-        """
-        cursor.execute(query, (person_name, current_year))
+        current_month = datetime.now().month
+        current_day = datetime.now().day
+
+        logging.debug(f"Checking if a message has been sent for {person_name} ({event_type}) on {current_year}-{current_month}-{current_day}")
+
+        # Birthday, Christmas, New Year -> Recurrence every year
+        if event_type in ['Birthday', 'Christmas', 'New Year']:
+            query = """
+            SELECT COUNT(*)
+            FROM message_log
+            WHERE Username = %s AND YEAR(date_sent) = %s
+            """
+            params = (person_name, current_year)
+            period = "year"
+
+        # baby and puppy -> Recurrence every month
+        elif event_type in ['baby', 'puppy']:
+            query = """
+            SELECT COUNT(*)
+            FROM message_log
+            WHERE Username = %s AND YEAR(date_sent) = %s AND MONTH(date_sent) = %s
+            """
+            params = (person_name, current_year, current_month)
+            period = "month"  # Adjust to reflect monthly recurrence
+
+        # nurturing -> Recurrence every two months
+        elif event_type == 'nurturing':
+            previous_two_months = (current_month - 2) % 12 or 12  # Handle month wrap-around
+            query = """
+            SELECT COUNT(*)
+            FROM message_log
+            WHERE Username = %s AND YEAR(date_sent) = %s 
+            AND MONTH(date_sent) BETWEEN %s AND %s
+            """
+            params = (person_name, current_year, previous_two_months, current_month)
+            period = "two months"
+
+        else:
+            logging.error(f"No message was sent for {person_name} - Event Type: {event_type} on {current_year}-{current_month}-{current_day}")
+            return False
+
+        cursor.execute(query, params)
         count = cursor.fetchone()[0]
         cursor.close()
-        return count > 0
 
-    def log_message_sent(self, cnx, person_name, event_type, message):
+        logging.debug(f"Message sent for {person_name} ({event_type}, message ID: {message_id}): {count}")
+
+        if count > 0:
+            logging.info(f"{event_type.capitalize()} message (ID: {message_id}) to {person_name} has already been sent this {period}.")
+            return True
+        return False
+
+    def log_message_sent(self, cnx, person_name, message_text):
         cursor = cnx.cursor()
         today = datetime.now().strftime('%Y-%m-%d')
-        query = "INSERT INTO message_log (Username, message, date_sent) VALUES (%s, %s, %s)"
-        cursor.execute(query, (person_name, message, today))
+
+        logging.debug(f"Logging message sent for {person_name}")
+
+        # Insert the message text into the message_log table
+        query = """
+        INSERT INTO message_log (Username, message, date_sent)
+        VALUES (%s, %s, %s)
+        """
+        
+        # Execute query with person_name, message_text, and today's date
+        cursor.execute(query, (person_name, message_text, today))
         cnx.commit()
         cursor.close()
 
-    def send_msg(self, username_receiver, msg):
+    
+    def get_last_sent_message(self, cnx, person_name):
+        """
+        Fetch the last message sent to the person from the message_log.
+        """
+        cursor = cnx.cursor()
+        query = """
+        SELECT message FROM message_log
+        WHERE Username = %s
+        ORDER BY date_sent DESC
+        LIMIT 1
+        """
         try:
+            cursor.execute(query, (person_name,))
+            result = cursor.fetchone()
+            if result:
+                return result[0]  # Return the message text
+            return None
+        except mysql.connector.Error as err:
+            logging.error(f"Error fetching last sent message: {err}")
+            return None
+        finally:
+            cursor.close()
+
+
+    def send_msg(self, username_receiver, Phone_Number, msg):
+        try:
+            logging.debug(f"Attempting to send message to {username_receiver} at {Phone_Number}: {msg}")
+
             with TelegramClient(self.session_file_name, self.app_id, self.app_hash) as client:
+                client.loop.run_until_complete(client.send_message(Phone_Number, msg))
+                logging.info(f"Message successfully sent to {username_receiver}.")
 
-                #loop = asyncio.get_event_loop()
-                #coroutine = client.get_entity("+351964395489")
-                #username = loop.run_until_complete(coroutine)
-
-                logging.info(f"client: %{username}")
-
-
-                client.loop.run_until_complete(client.send_message(username_receiver, msg))
-                logging.info(f"Message sent to {username_receiver}")
         except Exception as e:
             logging.error(f"Error sending message to {username_receiver}: {e}")
             return f"Error sending message to: {username_receiver}, error: {e}"
+        
         return "Success"
+
 
 
--- celebration_sql_connection.py
+++ celebration_sql_connection.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import logging
+import random
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(filename)s:%(lineno)d - %(asctime)s - %(levelname)s - %(message)s')
@@ -50,59 +51,79 @@ class CelebrationSqlConnection:
                 logging.error("Could not connect to the database")
                 return None
             
-    def get_contacts_with_event_types(self, cnx, table_name):
-        if cnx:
-            cursor = cnx.cursor()
-            query = f"SELECT Username, Message_receiver, Birthday_date, Recurrence, Type FROM {table_name}"
-            try:
-                cursor.execute(query)
-                rows = cursor.fetchall()
-                contacts_with_events = {}
-                
-                for row in rows:
-                    name = row[0]
-                    event_date = row[2].strftime('%Y-%m-%d')
-                    message_rec = row[1]
-                    recurrence = row[3]
-                    event_type = row[4]  # This is the new field added
-                    contacts_with_events[name] = (event_date, message_rec, recurrence, event_type)
-                    
-                cursor.close()
-                logging.debug(f"contacts_with_events: {contacts_with_events}")
-                return contacts_with_events
-            
-            except mysql.connector.Error as err:
-                logging.error(f"Error executing query: {err}")
-                cursor.close()
-                return None
-        else:
+    def get_contacts_for_today_events(self, cnx, table_name, today_month_day):
+        if not cnx:
             logging.error("Could not connect to the database")
             return None
 
-
-
+        cursor = cnx.cursor()
+        query = f"""
+        SELECT Username, Phone_Number, Birthday_date, Recurrence, Type 
+        FROM {table_name}
+        WHERE (Recurrence = 'Monthly' AND DATE_FORMAT(Birthday_date, '%d') = %s)
+        OR (Recurrence = 'Annual' AND DATE_FORMAT(Birthday_date, '%m-%d') = %s)
+        OR (Type IN ('Christmas', 'New Year'))
+        """
+        # Extract the day part for the 'Monthly' condition and the month-day part for the 'Annual' condition
+        today_day = today_month_day[-2:]  # Extract 'dd' for Monthly
+        today_month_day_formatted = today_month_day  # Full 'mm-dd' format for Annual
+        try:
+            cursor.execute(query, (today_day, today_month_day_formatted))
+            rows = cursor.fetchall()
+            contacts_with_events = {}
             
-    def get_event_message(self, cnx, messages_table, event_type):
+            for row in rows:
+                name, phone_number, event_date, recurrence, event_type = row
+                event_date_str = event_date.strftime('%Y-%m-%d') if event_date else None
+                contacts_with_events[name] = (event_date_str, phone_number, recurrence, event_type)
+            
+            logging.debug(f"contacts_with_events: {contacts_with_events}")
+            return contacts_with_events
+
+        except mysql.connector.Error as err:
+            logging.error(f"Error executing query: {err}")
+            return None
+
+        finally:
+            cursor.close()
+
+     
+    import random
+
+    def get_event_message(self, cnx, messages_table, event_type, last_message_text):
+        """
+        Fetch a random message for the given event type, excluding the last sent message.
+        """
         if cnx:
-            cursor = cnx.cursor()
-            query = f"SELECT text_message FROM {messages_table} WHERE Type = %s"
+            cursor = cnx.cursor(buffered=True)
+            query = f"SELECT id, text_message FROM {messages_table} WHERE Type = %s"
             try:
                 cursor.execute(query, (event_type,))
-                result = cursor.fetchone()
-                if result:
-                    ret = result[0]  # Assuming Message is the first column
-                    #cursor.close()
-                    return ret  
+                results = cursor.fetchall()
+                
+                # Filter out the last sent message
+                filtered_results = [result for result in results if result[1] != last_message_text]
+
+                if filtered_results:
+                    # Randomly select a message from the filtered list
+                    selected_message = random.choice(filtered_results)
+                    message_id = selected_message[0]
+                    message_text = selected_message[1]
+                    return message_id, message_text
                 else:
-                    logging.info(f"No message found for event type: {event_type}")
-                    return None
+                    logging.info(f"No alternative message found for event type: {event_type}")
+                    return None, None
             except mysql.connector.Error as err:
                 logging.error(f"Error executing query: {err}")
+                return None, None
+            finally:
                 cursor.close()
-                return None
         else:
             logging.error("Could not connect to the database")
-            return None
+            return None, None
+
+
+
 
 
 
